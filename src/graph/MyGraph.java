2bb98e4ea35625f103a3cb6c8c7e00c1e38f9f9c
assyl <zhangubek99@mail.ru>
2024-05-27 20:11:27 +0600

    implemented  MyGraph

--- src/graph/MyGraph.java
+++ src/graph/MyGraph.java
@@ -11,8 +11,48 @@ public class MyGraph<Vertex> {
     public MyGraph() {
         list = new HashMap<Vertex, List<Vertex>>();
     }
-    public void addVertex (Vertex vertex){
-        list.put(vertex,new ArrayList<>());
+
+    public void addVertex(Vertex vertex) {
+        list.put(vertex, new ArrayList<>());
+    }
+
+    public void addEdge(Vertex source, Vertex destination) {
+        checkVertex(destination);
+        checkVertex(source);
+        list.get(source).add(destination);
+        list.get(destination).add(source);
+    }
+
+    public boolean hasEdge(Vertex source, Vertex destination) {
+        checkVertex(destination);
+        checkVertex(source);
+        List<Vertex> neighbors = getNeighbours(source);
+        return neighbors != null && neighbors.contains(destination);
+    }
+
+    public void checkVertex(Vertex vertex) {
+        if (!list.containsKey(vertex)) {
+            throw new IndexOutOfBoundsException("Vertex" + vertex + "does not exist");
+        }
+    }
+
+    public void removeEdge(Vertex source, Vertex destination) {
+        checkVertex(destination);
+        checkVertex(source);
+        list.get(source).remove(destination);
+        list.get(destination).remove(source);
+    }
+
+    public List<Vertex> getNeighbours(Vertex vertex) {
+        List<Vertex> neighbour = list.get(vertex);
+        return neighbour;
+
+    }
+
+    public void printGraph() {
+        for (Vertex vertex : list.keySet()) {
+            System.out.print("Vertex " + vertex + " is connected to vertices" + getNeighbours(vertex));
+        }
     }
 
 }
