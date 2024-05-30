992a40bd395cc5f09bc908567ff4fe87fb63709b
assyl <zhangubek99@mail.ru>
2024-05-30 22:15:38 +0600

    Main class

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,62 @@
-import edge.Edge;
+import graph.MyGraph;
 import graph.WeightedGraph;
+import search.BreadthFirstSearch;
+import search.DepthFirstSearch;
+import search.DijkstraSearch;
+import search.Search;
 
 public class Main {
+
     public static void main(String[] args) {
-        /* Vertex<Integer> vertex = new Vertex<>(10);
-        Vertex<Integer> vertex1 = new Vertex<>(15);
-        Vertex<Integer> vertex2= new Vertex<>(20);
-        Vertex<Integer> vertex3= new Vertex<>(20);
-        Vertex<Integer> vertex4= new Vertex<>(20);
-        Vertex<Integer> vertex5= new Vertex<>(20);
+        WeightedGraph<String> weightedGraph = new WeightedGraph<>();
+        fillWithWeights(weightedGraph);
 
-        vertex1.addAdjVertices(vertex,4);
-        vertex2.addAdjVertices(vertex1,3);
-        vertex.addAdjVertices(vertex2,5);
-        vertex3.addAdjVertices(vertex4,1);
-        vertex4.addAdjVertices(vertex5,13);
-        vertex5.addAdjVertices(vertex1,22);
+        System.out.println("Dijkstra:");
+        Search<String> djk = new DijkstraSearch<>(weightedGraph, "Almaty");
+        outputPath(djk, "Kyzylorda");
 
 
-        vertex.printVertex();
-        vertex1.printVertex();
-        vertex2.printVertex();
-        System.out.println(vertex1.getData());
-        */
-        WeightedGraph<Integer> weightedGraph = new WeightedGraph<>();
-        weightedGraph.addVertex(10);
-        weightedGraph.addVertex(15);
-        weightedGraph.addVertex(13);
-        weightedGraph.addVertex(11);
-        weightedGraph.addEdge(10, 11, 56);
-        weightedGraph.addEdge(11, 13, 112);
-        weightedGraph.addEdge(13, 15, 112);
-        weightedGraph.printWeightedGraph();
-        System.out.println(weightedGraph.hasEdge(10, 15, 112));//result is false
+        System.out.println("---------------------------------------------------------------");
 
-        System.out.println(weightedGraph.getEdge(10));//result:[Edge{source=10, destination=11, weight=56.0}]
+        MyGraph<String> graph = new MyGraph<>();
+        fillWithoutWeights(graph);
 
+        System.out.println("DFS:");
+        Search<String> dfs = new DepthFirstSearch<>("Almaty", graph);
+        outputPath(dfs, "Kyzylorda");
+
+        System.out.println("------------------------------------------------------------------");
+
+        System.out.println("BFS:");
+        Search<String> bfs = new BreadthFirstSearch<>(graph, "Almaty");
+        outputPath(bfs, "Kyzylorda");
+    }
+
+    public static void fillWithoutWeights(MyGraph<String> graph) {
+        graph.addEdge("Almaty", "Astana");
+        graph.addEdge("Shymkent", "Atyrau");
+        graph.addEdge("Atyrau", "Astana");
+        graph.addEdge("Almaty", "Shymkent");
+        graph.addEdge("Shymkent", "Astana");
+        graph.addEdge("Astana", "Kostanay");
+        graph.addEdge("Shymkent", "Kyzylorda");
+    }
+
+    public static void fillWithWeights(WeightedGraph<String> graph) {
+        graph.addEdge("Almaty", "Astana", 2.1);
+        graph.addEdge("Shymkent", "Atyrau", 7.8);
+        graph.addEdge("Atyrau", "Astana", 7.1);
+        graph.addEdge("Almaty", "Shymkent", 7.2);
+        graph.addEdge("Shymkent", "Astana", 3.9);
+        graph.addEdge("Astana", "Kostanay", 3.5);
+        graph.addEdge("Shymkent", "Kyzylorda", 5.4);
+    }
+
+    public static void outputPath(Search<String> search, String key) {
+        for (String v : search.pathTo(key)) {
+            System.out.print(v + " -> ");
+        }
+
+        System.out.println();
     }
 }
